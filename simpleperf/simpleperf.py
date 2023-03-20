a9078d3895b736fa606bd70f4759cb7c73c3fb50
vejmon <vemundhell@gmail.com>
2023-03-20 14:22:30 +0100

    faen

--- simpleperf/simpleperf.py
+++ simpleperf/simpleperf.py
@@ -1,25 +1,49 @@
+# import re
+import subprocess
 import sys
 import argparse
-import re
 import socket
 import threading as th
+import ipaddress
 
+
+# function to check run ifconfig on a node, and get it's ip address
+def get_ip():
+    # Run the ifconfig command and capture the output
+    output = subprocess.check_output(['ifconfig'])
+
+    # Decode the output from bytes to a string
+    output_str = output.decode()
+
+    # Split the output into lines
+    output_lines = output_str.split('\n')
+
+    # Find the line that contains the IP address
+    for line in output_lines:
+        if 'inet ' in line:
+            ip_address = line.split(' ')[1]
+            break
+    print(ip_address)
+    return ip_address
 
 # function to validate the ip address given as an argument, validatet with a regex pattern.
 # if we don't have a valid ip address we terminate the program
 def valid_ip(inn):  # ip address must start with 1-3 digits seperated by a dot, repeated three more times.
-    ip_regex = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
-    if ip_regex.match(inn):
-        return inn
-    else:
-        raise ValueError(f"{inn} is not a valid ip address.\nRequired dotted decimal notation.\nExample: 3.3.3.3")
+    try:
+        ip = ipaddress.ip_address(inn)
+    except ValueError:
+        print(f"{inn} is not a valid ip address.\nRequired dotted decimal notation.\nExample: 3.3.3.3")
+        sys.exit(1)
+        # cast the ipv4 apress to string
+    return str(ip)
+    # ip_regex = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
 
 
 # check if port is an integer and between 1024 - 65535
 def valid_port(inn):
     try:
         ut = int(inn)
-    except ValueError:
+    except TypeError:
         raise argparse.ArgumentTypeError(f"port must be an integer, {inn} isn't")
     if not (1024 <= ut <= 65535):
         raise argparse.ArgumentTypeError(f'port number: ({inn}) must be within range [1024 - 65535]')
@@ -47,8 +71,6 @@ def valid_num(inn):
         raise argparse.ArgumentTypeError(f"bytes must be a positive integer, {inn} isn't")
     return ut
 
-
-
 # start the argument parser
 parse = argparse.ArgumentParser(description="optional arguments for simpleperf", epilog='simpleperf --help')
 
@@ -58,13 +80,13 @@ parse.add_argument('-c', '--client', action='store_true', help='enables client m
 parse.add_argument('-p', '--port', type=valid_port, default=8088, help="which port to bind/open")
 
 # server arguments ignored if running a client
-parse.add_argument('-b', '--bind', type=valid_ip, default=socket.gethostbyname(socket.gethostname()),
+parse.add_argument('-b', '--bind', type=valid_ip, default=get_ip(),
                    help="ipv4 adress to bind server to")
 parse.add_argument('-f', '--format', choices=['B', 'KB', 'MB', 'GB', 'TB'],
                    type=str, default='MB', help="format output with SI prefix")
 
 # client arguments ignored if running a server
-parse.add_argument('-I', '--serverip', type=valid_ip, default=socket.gethostbyname(socket.gethostname()),
+parse.add_argument('-I', '--serverip', type=valid_ip, default=get_ip(),
                    help="ipv4 address to connect with")
 parse.add_argument('-t', '--time', type=valid_time, default=25, help="time duration to transfer bytes")
 parse.add_argument('-i', '--interval', type=valid_time, default=25, help='intervall between prints to consoll')
@@ -79,7 +101,7 @@ args = parse.parse_args()
 
 
 def handle_client(con, cli_arg):
-
+    print("hallo")
 
 
 # an instance of simpleperf may only be server or client
@@ -93,7 +115,7 @@ def server():
     serv_port = int(args.port)
     serv_ip = args.bind
 
-        # open a socket using ipv4 address(AF_INET), and a TCP connection (SOCK_STREAM)
+    # open a socket using ipv4 address(AF_INET), and a TCP connection (SOCK_STREAM)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serv:
         # attempt to bind to port, quit if failed.
         try:
@@ -105,7 +127,7 @@ def server():
         serv.listen(15)
         print(f"{dashes}\n   a simpleperf server is listening on port <{serv_ip}:{serv_port}>\n{dashes}")
 
-        #accepts a connection and start a thread handling the connection.
+        # accepts a connection and start a thread handling the connection.
         while True:
             # accepts an incoming client and deliver requested document.
             con, rport = serv.accept()
@@ -116,9 +138,7 @@ def server():
             t = th.Thread()
 
 
-
 def client():
-
     # open a socket using ipv4 address(AF_INET), and a TCP connection (SOCK_STREAM)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cli:
         serv_ip = args.serverip
@@ -126,7 +146,7 @@ def client():
 
         print(f"{serv_ip}:{serv_port}")
 
-        #build a byte message which is 1000 B aprox 1 KB
+        # build a byte message which is 1000 B aprox 1 KB
         msg = ("wop!" * 250).encode()
 
         # set a timeout timer.
@@ -136,6 +156,7 @@ def client():
             con = cli.connect((serv_ip, serv_port))
         except ConnectionError:
             raise ConnectionError("connection failed")
+
 
 # bla
 
